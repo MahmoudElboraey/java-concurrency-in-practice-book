dff26ff4c80ab65d28d11fac169bb6cf08c60679
Mahmoud Elborey <mahmoudbubg1234@gmail.com>
2026-08-26 20:37:48 +0300

    first reading sesstion of chapter 03

--- chapter-03-sharing-objects/src/codeexamples/NoVisibility.java
+++ chapter-03-sharing-objects/src/codeexamples/NoVisibility.java
@@ -1,4 +1,25 @@
 package codeexamples;
 
 public class NoVisibility {
+
+    private static boolean ready;
+    private static int number;
+
+
+    public static void main(String[] args) {
+        new ReaderThread().start();
+        number = 42;
+        ready = true;
+
+    }
+
+
+    public static class ReaderThread extends Thread {
+        public void run (){
+            while (!ready) {
+                Thread.yield();
+            }
+            System.out.println(number);
+        }
+    }
 }
