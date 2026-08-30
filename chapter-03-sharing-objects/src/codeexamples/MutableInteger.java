ccdfbcad9ab82dc248b8a0d47a5af3fd7b0a1235
Mahmoud Elborey <mahmoudbubg1234@gmail.com>
2026-08-30 20:13:46 +0300

    reading about visibility & volatile variables

---
+++ chapter-03-sharing-objects/src/codeexamples/MutableInteger.java
@@ -0,0 +1,17 @@
+package codeexamples;
+
+
+import net.jcip.annotations.NotThreadSafe;
+
+@NotThreadSafe
+public class MutableInteger {
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
